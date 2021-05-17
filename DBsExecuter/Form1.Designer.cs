af263a2ac814ba428fa4fa17536b96f01d1f28ba
iluxa1810 <iluxa1810@gmail.com>
2021-05-17 22:35:29 +0300

    Update Form1.Designer.cs

--- DBsExecuter/Form1.Designer.cs
+++ DBsExecuter/Form1.Designer.cs
@@ -3,7 +3,7 @@
     partial class Form1
     {
         /// <summary>
-        /// Required designer variable12.
+        /// Required designer variable14.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
